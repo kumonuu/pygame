530c4b9fb753c36c579adab81c18e6ce92629896
kumonuu <kumonuu@proton.me>
2025-03-15 17:01:42 +0000

    Add files via upload

--- space_invaders/game.py
+++ space_invaders/game.py
@@ -4,6 +4,8 @@ import sys
 WIDTH = 1000
 HEIGHT = 600
 
+pygame.init()
+
 # make screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -31,6 +33,10 @@ def display_on_screen():
     for bullet in red_bullets:
         pygame.draw.rect(screen, "red", bullet)
         bullet.x -= 2
+    text = font.render(str(yellow_health), True, "white")
+    screen.blit(text, (240, 30))
+    text2 = font.render(str(red_health), True, "white")
+    screen.blit(text2, (740,30))
 
 def move_shooter():
     key_press = pygame.key.get_pressed()
@@ -63,21 +69,64 @@ def spawn_bullet_red():
     bullet = pygame.rect.Rect(red_rect.x+30,red_rect.y+30,7,4)
     red_bullets.append(bullet)
 
+def bullet_collision_check():
+    global yellow_health
+    global red_health
+    for yellow_bullet in yellow_bullets:
+        if yellow_bullet.colliderect(red_rect):
+            yellow_bullets.remove(yellow_bullet)
+            red_health -= 1
+    for red_bullet in red_bullets:
+        if red_bullet.colliderect(yellow_rect):
+            red_bullets.remove(red_bullet)
+            yellow_health -= 1
+
+def decide_the_winner():
+    if yellow_health < 1:
+        text3 = font.render("Red wins!", True, "white")
+        screen.fill("black")
+        screen.blit(text3, (WIDTH/2,50))
+        restart_text()
+    elif red_health < 1:
+        text3 = font.render("Yellow wins!", True, "white")
+        screen.fill("black")
+        screen.blit(text3, (WIDTH/2,50))
+        restart_text()
+
+def restart_text():
+    text = font.render("Press R to Restart game", True, "white")
+    screen.blit(text, (WIDTH/2, 70))
+
+def restart_game():
+    global yellow_health
+    global red_health
+    display_on_screen()
+    yellow_health = 10
+    red_health = 10
+
 yellow_bullets = []
 red_bullets = []
 border = pygame.rect.Rect(WIDTH/2,0,5,600)
 yellow_rect = pygame.rect.Rect(200,250,60,60)
 red_rect = pygame.rect.Rect(750,250,60,60)
+yellow_health = 10
+red_health = 10
+font = pygame.font.SysFont("Calibri", 30)
+text = 0
 
 while True:
     display_on_screen()
     move_shooter()
+    bullet_collision_check()
+    decide_the_winner()
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LSHIFT:
                 spawn_bullet_yellow()
             elif event.key == pygame.K_RSHIFT:
                 spawn_bullet_red()
+            elif event.key == pygame.K_r:
+                restart_game()
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
